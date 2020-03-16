21cebc9ba20af3e814c8b72ac834dc16da164070
Lazloian <henry.silva.999@gmail.com>
2020-03-16 15:01:30 -0400

    added multiplexer function

---
+++ tcaMulti.c
@@ -0,0 +1,8 @@
+#define TCA_DEV_ADD	0x70
+
+void tcaSelect(unsigned char channels)
+{
+	unsigned char command[1];
+	command[0] = channels;
+    i2cStats = HAL_I2C_Mem_Write(&hi2c1, TCA_DEV_ADD << 1, 0x00, I2C_MEMADD_SIZE_8BIT, command, 1, 100);
+}
